15da416ce59513d95675227f2adcf2103e64c880
ispan <ispan@DESKTOP-PAKSETB>
2022-10-25 16:42:36 +0800

    加入專案檔案。

---
+++ Exec2_Star_/Form1.Designer.cs
@@ -0,0 +1,140 @@
+﻿namespace Exec2_Star_
+{
+	partial class Form1
+	{
+		/// <summary>
+		/// 設計工具所需的變數。
+		/// </summary>
+		private System.ComponentModel.IContainer components = null;
+
+		/// <summary>
+		/// 清除任何使用中的資源。
+		/// </summary>
+		/// <param name="disposing">如果應該處置受控資源則為 true，否則為 false。</param>
+		protected override void Dispose(bool disposing)
+		{
+			if (disposing && (components != null))
+			{
+				components.Dispose();
+			}
+			base.Dispose(disposing);
+		}
+
+		#region Windows Form 設計工具產生的程式碼
+
+		/// <summary>
+		/// 此為設計工具支援所需的方法 - 請勿使用程式碼編輯器修改
+		/// 這個方法的內容。
+		/// </summary>
+		private void InitializeComponent()
+		{
+			this.rowlbl = new System.Windows.Forms.Label();
+			this.rowText = new System.Windows.Forms.TextBox();
+			this.inputbtn = new System.Windows.Forms.Button();
+			this.inputbtn2 = new System.Windows.Forms.Button();
+			this.inputbtn3 = new System.Windows.Forms.Button();
+			this.starText = new System.Windows.Forms.TextBox();
+			this.starLabel = new System.Windows.Forms.Label();
+			this.SuspendLayout();
+			// 
+			// rowlbl
+			// 
+			this.rowlbl.AutoSize = true;
+			this.rowlbl.Location = new System.Drawing.Point(127, 70);
+			this.rowlbl.Name = "rowlbl";
+			this.rowlbl.Size = new System.Drawing.Size(41, 12);
+			this.rowlbl.TabIndex = 0;
+			this.rowlbl.Text = "列數：";
+			this.rowlbl.Click += new System.EventHandler(this.rowlbl_Click);
+			// 
+			// rowText
+			// 
+			this.rowText.Location = new System.Drawing.Point(194, 66);
+			this.rowText.MaxLength = 2;
+			this.rowText.Name = "rowText";
+			this.rowText.Size = new System.Drawing.Size(100, 22);
+			this.rowText.TabIndex = 1;
+			// 
+			// inputbtn
+			// 
+			this.inputbtn.Location = new System.Drawing.Point(387, 65);
+			this.inputbtn.Name = "inputbtn";
+			this.inputbtn.Size = new System.Drawing.Size(75, 23);
+			this.inputbtn.TabIndex = 2;
+			this.inputbtn.Text = "靠左三角形";
+			this.inputbtn.UseVisualStyleBackColor = true;
+			this.inputbtn.Click += new System.EventHandler(this.inputbtn_Click);
+			// 
+			// inputbtn2
+			// 
+			this.inputbtn2.Location = new System.Drawing.Point(483, 65);
+			this.inputbtn2.Name = "inputbtn2";
+			this.inputbtn2.Size = new System.Drawing.Size(75, 23);
+			this.inputbtn2.TabIndex = 3;
+			this.inputbtn2.Text = "靠右三角形";
+			this.inputbtn2.UseVisualStyleBackColor = true;
+			this.inputbtn2.Click += new System.EventHandler(this.inputbtn2_Click);
+			// 
+			// inputbtn3
+			// 
+			this.inputbtn3.Location = new System.Drawing.Point(579, 65);
+			this.inputbtn3.Name = "inputbtn3";
+			this.inputbtn3.Size = new System.Drawing.Size(75, 23);
+			this.inputbtn3.TabIndex = 4;
+			this.inputbtn3.Text = "等腰三角形";
+			this.inputbtn3.UseVisualStyleBackColor = true;
+			this.inputbtn3.Click += new System.EventHandler(this.inputbtn3_Click);
+			// 
+			// starText
+			// 
+			this.starText.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+			this.starText.Location = new System.Drawing.Point(129, 128);
+			this.starText.Multiline = true;
+			this.starText.Name = "starText";
+			this.starText.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
+			this.starText.Size = new System.Drawing.Size(525, 123);
+			this.starText.TabIndex = 5;
+			// 
+			// starLabel
+			// 
+			this.starLabel.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+			this.starLabel.Location = new System.Drawing.Point(127, 266);
+			this.starLabel.Name = "starLabel";
+			this.starLabel.Size = new System.Drawing.Size(527, 146);
+			this.starLabel.TabIndex = 6;
+			this.starLabel.Text = "starLabel";
+			// 
+			// Form1
+			// 
+			this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+			this.ClientSize = new System.Drawing.Size(800, 450);
+			this.Controls.Add(this.starLabel);
+			this.Controls.Add(this.starText);
+			this.Controls.Add(this.inputbtn3);
+			this.Controls.Add(this.inputbtn2);
+			this.Controls.Add(this.inputbtn);
+			this.Controls.Add(this.rowText);
+			this.Controls.Add(this.rowlbl);
+			this.Name = "Form1";
+			this.Text = "Form1";
+			this.ResumeLayout(false);
+			this.PerformLayout();
+
+		}
+
+		#endregion
+
+		private System.Windows.Forms.Label rowlbl;
+		private System.Windows.Forms.TextBox rowText;
+		private System.Windows.Forms.Button inputbtn;
+		private System.Windows.Forms.Button inputbtn2;
+		private System.Windows.Forms.Button inputbtn3;
+		private System.Windows.Forms.TextBox starText;
+		private System.Windows.Forms.Label starLabel;
+	}
+}
+
---
+++ Exec2_Star_/Form1.cs
@@ -0,0 +1,158 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Exec2_Star_
+{
+	public partial class Form1 : Form
+	{
+		public Form1()
+		{
+			InitializeComponent();
+			starLabel.Text = string.Empty;
+		}
+
+		private void inputbtn_Click(object sender, EventArgs e)
+		{
+			int rows=0;
+			try
+			{
+				 rows = Getrows();
+
+			}
+			catch(Exception ex)
+			{
+				MessageBox.Show(ex.Message);
+			}
+			string result = GenerateStar(rows); //rows會錯
+
+			//放入TextBox
+			DisplayStar(result);
+		}
+
+		private void inputbtn2_Click(object sender, EventArgs e)
+		{
+			int rows = 0;
+			try
+			{
+				rows = Getrows();
+
+			}
+			catch (Exception ex)
+			{
+				MessageBox.Show(ex.Message);
+			}
+			string result = GenerateLeftStar(rows); //rows會錯
+
+			//放入TextBox
+			DisplayStar(result);
+		}
+
+		private void inputbtn3_Click(object sender, EventArgs e)
+		{
+			int rows = 0;
+			try
+			{
+				rows = Getrows();
+
+			}
+			catch (Exception ex)
+			{
+				MessageBox.Show(ex.Message);
+			}
+			string result = GenerateSameStar(rows); //rows會錯
+
+			//放入TextBox
+			DisplayStar(result);
+		}
+
+		private int Getrows()
+		{
+			bool isNumber = int.TryParse(rowText.Text, out int rows);
+			if (isNumber == false)
+			{
+				throw new Exception("請輸入列數");
+			}
+			if(rows <= 0)
+			{
+				throw new Exception("請輸入正整數");
+			}
+			if (rows > 10)
+			{
+				throw new Exception("請輸入1~10的數字");
+			}
+			return rows;
+		}
+
+		private string GenerateStar(int rows)
+		{
+			string result = string.Empty;
+			for (int i = 1; i <= rows; i++)
+			{
+				result += new string('*', i) + "\r\n"; //+=忘記了
+			}
+			return result; //記得要return
+		}
+		private string DisplayStar(string star)
+		{
+			starText.Text = star;
+			starLabel.Text = star;
+			return star;
+		}
+		private string GenerateLeftStar(int rows)
+		{
+			string result = string.Empty;
+			int star = 0;
+			string[] empty = { string.Empty, "1" };
+			for (int i = rows; 0 < i; i--)
+			{
+				star++;
+				result += new string(' ', i) + new string(' ', i) + new string('*', star) + "\r\n";
+			}
+			return result;
+
+		}
+		private string GenerateSameStar(int rows)
+		{
+			string result = string.Empty;
+			int star = 1;
+			for (int i = rows - 1; 0 <= i; i--)
+			{
+				result += new string(' ', i) + new string(' ', i) + new string('*', star) + "\r\n";
+				star += 2;
+			}
+			return result;
+		}
+
+		private void rowlbl_Click(object sender, EventArgs e)
+		{
+
+		}
+		//取列數
+		//int? rows = GetRows(); //若要顯示null要+?
+		//	if (rows.HasValue == false) //null
+		//	{
+		//		MessageBox.Show("請輸入列數");
+		//		return;
+		//	}
+
+		//	//判斷>0
+		//	if(rows.Value <= 0)
+		//	{
+		//		MessageBox.Show("請輸入正整數");
+		//		return ;
+		//	}
+		//private int? GetRows() //若要顯示null要+? 要回傳都要有東西
+		//{
+		//	bool isInt = int.TryParse(rowText.Text, out int rows);
+		//	if (isInt) { return rows; }
+		//	else { return null; }
+		//}
+	}
+}
